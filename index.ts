5e4bc424835533e314bbd8d13c3865bd18362b16
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-13 20:50:59 +0300

    connected to the db

--- index.ts
+++ index.ts
@@ -6,6 +6,26 @@ import { schema } from "./Schema";
 
 const app = express();
 
+if (!process.env.MONGO_URI) {
+  throw new Error("Mongo URI must be provided");
+}
+
+const mongooseConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("connected to db");
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+mongooseConnect();
+
 app.use(
   "/graphql",
   graphqlHTTP({
