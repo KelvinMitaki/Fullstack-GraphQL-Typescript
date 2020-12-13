db41bb6f41a5a28379cab66fd53826a0bcd82986
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-13 18:14:04 +0300

    basic graphql setup

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import connectMongo from "connect-mongo";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./Schema";
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema
+  })
+);
+
+app.listen(4000, () => console.log("server started on port 4000"));
