fc7497ff07b235af0873832192783336a89b469c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-14 16:24:16 +0300

    added header styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { useQuery } from "react-apollo";
 import { currentUser } from "../queries/currentUser";
+import styles from "./Header.module.css";
 
 const Header: React.FC = props => {
   const { data, loading } = useQuery(currentUser);
   if (loading) {
     console.log({ loading });
-  } else {
-    console.log(data);
   }
-  return <div>Header Header</div>;
+  if (data && data.user) {
+    return (
+      <div className={styles.header}>{JSON.stringify(data.user, null, 4)}</div>
+    );
+  }
+  return <div className={styles.header}>You are not logged in</div>;
 };
 
 export default Header;
