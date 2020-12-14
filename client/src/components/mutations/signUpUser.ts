30adb4859e8665bea89d28331ee3e3fabbb5103e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-14 22:38:28 +0300

    bug fix

--- client/src/components/mutations/signUpUser.ts
+++ client/src/components/mutations/signUpUser.ts
@@ -1,8 +1,8 @@
 import { gql } from "apollo-boost";
 
 export const signupUser = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation Signup($email: String!, $password: String!) {
+    signup(email: $email, password: $password) {
       email
       _id
     }
