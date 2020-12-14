047ac9ba3e2cc054af9c2bd9b8746ffb8f3b86da
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-14 13:02:12 +0300

    added header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Route path="/" exact component={HomePage} />
     </div>
   );
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Header Header</div>;
+};
+
+export default Header;
