cc2c19e3743b15254ccbddfc83070f93ebaf2d10
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-14 16:35:22 +0300

    wiredup home navigation

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { useQuery } from "react-apollo";
+import { RouteComponentProps, withRouter } from "react-router-dom";
 import { currentUser } from "../queries/currentUser";
 import styles from "./Header.module.css";
 
-const Header: React.FC = props => {
+const Header: React.FC<RouteComponentProps> = props => {
   const { data, loading } = useQuery(currentUser);
   if (loading) {
     console.log({ loading });
   }
   if (data && data.user) {
     return (
-      <div className={styles.header}>{JSON.stringify(data.user, null, 4)}</div>
+      <div className={styles.header}>
+        <h2 onClick={() => props.history.push("/")}>Home</h2>
+        <div>{JSON.stringify(data.user, null, 4)}</div>
+      </div>
     );
   }
-  return <div className={styles.header}>You are not logged in</div>;
+  return (
+    <div className={styles.header}>
+      <h2 onClick={() => props.history.push("/")}>Home</h2>
+      <div>You are not logged in</div>
+    </div>
+  );
 };
 
-export default Header;
+export default withRouter(Header);
